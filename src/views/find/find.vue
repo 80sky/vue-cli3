7257cc309d78fdb104d6797871a1adca5851f675
sunny.li <liwenbin19880121@gmail.com>
2019-01-22 15:56:06 +0800

    更新

--- src/views/find/find.vue
+++ src/views/find/find.vue
@@ -1,5 +1,40 @@
 <template>
-  <div>
-    find
+  <div class="find">
+    <van-cell-group class="find_group">
+      <van-cell title="朋友圈" is-link icon="friends" />
+    </van-cell-group>
+    <van-cell-group class="find_group">
+      <van-cell title="扫一扫" is-link icon="friends" />
+      <van-cell title="摇一摇" is-link icon="friends" />
+    </van-cell-group>
+    <van-cell-group class="find_group">
+      <van-cell title="看一看" is-link icon="friends" />
+      <van-cell title="搜一搜" is-link icon="friends" />
+    </van-cell-group>
+    <van-cell-group class="find_group">
+      <van-cell title="附近的人" is-link icon="friends" />
+      <van-cell title="漂流瓶" is-link icon="friends" />
+    </van-cell-group>
+    <van-cell-group class="find_group">
+      <van-cell title="购物" is-link icon="friends" />
+      <van-cell title="游戏" is-link icon="friends" />
+    </van-cell-group>
+    <van-cell-group class="find_group">
+      <van-cell title="小程序" is-link icon="friends" />
+    </van-cell-group>
   </div>
 </template>
+
+<style lang="less">
+.find {
+  background: #e5e5e5;
+  height: calc(~"100% - 1.333333rem");
+  overflow: scroll;
+  .find_group {
+    margin-top: 10px;
+    &:first-child {
+      margin-top: 0;
+    }
+  }
+}
+</style>
