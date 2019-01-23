fb1418f3802cb6339c2fae07cebf7b5f5e35e67c
sunny.li <liwenbin19880121@gmail.com>
2019-01-23 16:55:20 +0800

    更新配置

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <template>
   <div id="app">
-    <van-tabbar v-model="active"
-      active-color="#09bb07">
-      <van-tabbar-item icon="chat"  :to="{name:'home'}">微信</van-tabbar-item>
-      <van-tabbar-item icon="friends" :to="{name:'phone'}">通讯录</van-tabbar-item>
-      <van-tabbar-item icon="eye" :to="{name:'find'}">发现</van-tabbar-item>
-      <van-tabbar-item icon="setting-o" :to="{name:'my'}">我</van-tabbar-item>
+    <van-tabbar v-model="active" active-color="#09bb07">
+      <van-tabbar-item icon="chat" :to="{ name: 'home' }">微信</van-tabbar-item>
+      <van-tabbar-item icon="friends" :to="{ name: 'phone' }">通讯录</van-tabbar-item>
+      <van-tabbar-item icon="eye" :to="{ name: 'find' }">发现</van-tabbar-item>
+      <van-tabbar-item icon="setting-o" :to="{ name: 'my' }">我</van-tabbar-item>
     </van-tabbar>
     <router-view/>
   </div>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,21 +1,21 @@
 <template>
   <div class="home">
-    <van-cell v-for="(item,index) in chatList" :key="index">
+    <van-cell v-for="(item, index) in chatList" :key="index">
       <template slot="icon">
         <img class="h_img" :src="item.imgUrl">
       </template>
       <template slot="title">
         <van-row>
           <van-col span="20" class="custom-content">
-            <div class="custom-text">{{item.title}}</div>
-            <div class="custom-desc">{{item.desc}}</div>
+            <div class="custom-text">{{ item.title }}</div>
+            <div class="custom-desc">{{ item.desc }}</div>
           </van-col>
           <van-col span="4" class="time">下午2:20</van-col>
         </van-row>
       </template>
     </van-cell>
   </div>
-</template>   
+</template>
 
 <script>
 import API_SERVER from "@/server/api/user";
